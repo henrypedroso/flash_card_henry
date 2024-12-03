ee30dcd37c757f87f28ad54a8053342bfb83c83d
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-12-03 14:17:57 -0300

    sssssssssssssss

---
+++ assets/perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Robótica',
+    'O que é um Robô?',
+    'Um artefato que executa uma tarefa programada'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a molécula da vida?',
+    'A molécula da vida é o DNA'
+)
+
