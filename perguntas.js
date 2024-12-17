0da63e9f80d7af29dd2fc0d60013dc51e9d6b847
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-12-17 16:25:09 -0300

    ssssssssssssssssss

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,9 @@ criaCartao(
     'Qual a molécula da vida?',
     'A molécula da vida é o DNA.'
 )
+
+criaCartao(
+    'Biologia',
+    'Qual a molécula da vida?',
+    'A molécula da vida é o DNA.'
+)
