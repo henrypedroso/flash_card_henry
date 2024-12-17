f2578ee3b09fdd2342c0ce3f617ff37911ffdfaf
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-12-17 08:12:03 -0300

    sssssssssssssssssssssss

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ function criaCartao(categoria, pergunta, resposta) {
 
     function viraCartao(){
         respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active, respostaEstaVisivel')
     }
+
+    cartao.addEventListener('click', viraCartao );
 
 
 
